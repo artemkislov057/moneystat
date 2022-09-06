12738883fe52916ea9f3523605cb1e0c326021a0
a.kislov <a.kislov@skbkontur.ru>
2022-09-06 20:19:25 +0500

    добавил сваггер

--- backend/MoneyStat.WebApi/MoneyStat.WebApi.Api/Startup.cs
+++ backend/MoneyStat.WebApi/MoneyStat.WebApi.Api/Startup.cs
@@ -6,15 +6,20 @@ public sealed class Startup
     {
         services.AddRouting();
         services.AddControllers();
+        services.AddSwaggerGen();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = "swagger";
+        });
+
         app.UseRouting();
 
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
 }
