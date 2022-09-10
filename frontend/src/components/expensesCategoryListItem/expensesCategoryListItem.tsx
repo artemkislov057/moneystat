de7a8ab46e3792c433a894ebce2033fc7c8b6915
dimasikBurdin <mityabur@gmail.com>
2022-09-10 19:41:14 +0500

    добавлена функциональность category list item

--- frontend/src/components/expensesCategoryListItem/expensesCategoryListItem.tsx
+++ frontend/src/components/expensesCategoryListItem/expensesCategoryListItem.tsx
@@ -15,13 +15,21 @@ export const CategoryListItem:React.FC<TProps> = React.memo((props) => {
     return <div className="category-list-item">
         <div className="category-list-item-name-container">
             <span className={`category-list-item-name-icon ${props.type}`}></span>
-            <span className="category-list-item-name-title">{ExpensesCategoryValue[props.type]}</span>
+            <div className="category-list-item-name-title-container">
+                <span className="category-list-item-name-title">{ExpensesCategoryValue[props.type]}</span>
+                {props.date &&
+                    <span className="category-list-item-name-date">{props.date}</span>
+                }
+            </div>
         </div>
         <div className="category-list-item-price">
             <span className="category-list-item-price-value">
                 {props.typePrice === 'expenses' && <>-</>}
                 {props.price} ₽
             </span>
+            {props.onClickDelete &&
+                <button className="category-list-item-price-delete-button" onClick={() => props.onClickDelete()}></button>
+            }
         </div>
     </div>
 })
