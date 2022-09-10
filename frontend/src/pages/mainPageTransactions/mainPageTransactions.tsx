75b4c435f364ac505ded10230e493cfdf49dcadc
dimasikBurdin <mityabur@gmail.com>
2022-09-11 00:21:49 +0500

    add mui pedendencies

--- frontend/src/pages/mainPageTransactions/mainPageTransactions.tsx
+++ frontend/src/pages/mainPageTransactions/mainPageTransactions.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { TestDiagram } from "../../components/testDiagram/testDiagram";
 import './mainPageTransactions.css';
 
 export const MainPageTransactions:React.FC = () => {
     return <div className="main-page-transactions-container">
-        transactions
+        {/* <TestDiagram /> */}
     </div>
 }
