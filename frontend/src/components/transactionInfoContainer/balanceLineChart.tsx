c95c25b230dedfbce6bcde5a36b5c61ba86c182b
dimasikBurdin <mityabur@gmail.com>
2022-09-16 17:11:56 +0500

    fix font family, add transaction bar chart

--- frontend/src/components/transactionInfoContainer/balanceLineChart.tsx
+++ frontend/src/components/transactionInfoContainer/balanceLineChart.tsx
@@ -8,10 +8,10 @@ type TProps = {
 
 export const BalanceLineChart:React.FC<TProps> = (props) => {
     const data: ChartData<'line'> = {
-        labels: [1,2,3,4,5,6],
+        labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
         datasets: [
             {
-                data: [4000,7000,8000,9000,5000,6005],
+                data: [4000,7000,8000,9000,5000,6005,2345,2314,2333,3545,232,3,5444,633,1221,6,687,],
                 
                 fill: {
                     target: 'origin',
@@ -27,7 +27,6 @@ export const BalanceLineChart:React.FC<TProps> = (props) => {
     }
 
     return <div 
-        className="test-diagram" 
         style={{width: '100%', height: '100%'}}
     >
         <Line 
@@ -45,6 +44,11 @@ export const BalanceLineChart:React.FC<TProps> = (props) => {
                         x: {
                             grid: {
                                 display: false
+                            }
+                        },
+                        y: {
+                            grid: {
+                                borderDash: [2,5]
                             }
                         }
                     }
--- frontend/src/components/transactionInfoContainer/transactionBarChart.tsx
+++ frontend/src/components/transactionInfoContainer/transactionBarChart.tsx
@@ -1,9 +1,52 @@
+import { ChartData } from "chart.js";
 import React from "react";
+import { Bar } from "react-chartjs-2";
 
 type TProps = {
     
 }
 
 export const TransactionBarChart:React.FC = (props) => {
-    return <></>
+    const data: ChartData<'bar'> = {
+        labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+        datasets: [
+            {                
+                data: [4000,7000,8000,9000,5000,6005,2,3,3434,1,2,45,45,4,4,5656,3, 150000],
+                backgroundColor: '#4851FB'
+                
+               
+            }
+        ],
+    }
+
+    return <div 
+        style={{width: '100%', height: '100%'}}
+    >
+        <Bar 
+            data={
+                data
+            }
+            options={{
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false
+                        },                        
+                    },
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false
+                            }
+                        },
+                        y: {
+                            grid: {
+                                borderDash: [2,5]
+                            }
+                        }
+                    }
+                }
+            }
+        />
+    </div>
 }
